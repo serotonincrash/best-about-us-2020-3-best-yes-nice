98d3a8d5fc43cabc4ec706bacf68b3860905b990
Sean Wong <derpydm@gmail.com>
2023-05-31 13:41:22 +0800

    Initial Commit

---
+++ best-about-us-2020-3-best-yes-nice/best_about_us_2020_3_best_yes_niceApp.swift
@@ -0,0 +1,17 @@
+//
+//  best_about_us_2020_3_best_yes_niceApp.swift
+//  best-about-us-2020-3-best-yes-nice
+//
+//  Created by Sean Wong on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct best_about_us_2020_3_best_yes_niceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

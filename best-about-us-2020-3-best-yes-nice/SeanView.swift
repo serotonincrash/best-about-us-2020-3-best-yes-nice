04564569f6081dc44cd1943291ec949c4faa1501
Sean Wong <derpydm@gmail.com>
2023-05-31 14:03:11 +0800

    add rickroll

--- best-about-us-2020-3-best-yes-nice/SeanView.swift
+++ best-about-us-2020-3-best-yes-nice/SeanView.swift
@@ -7,19 +7,33 @@
 
 import SwiftUI
 import Font
+import Subsonic
 
 struct SeanView: View {
     var body: some View {
         ZStack {
             Color.clear
             VStack {
+                Image("sean")
+                    .resizable()
+                    .mask {
+                        Circle()
+                    }
+                    .scaledToFit()
+                    .frame(width:200)
+                    .padding(.all)
                 Text("Sean Wong")
                     .font(.custom("Papyrus", size: 64))
                     .foregroundColor(.red)
+                    .bold()
+                    .strikethrough()
+                    .padding()
             }
         }.background(
             RadialGradient(colors: [.white,.blue,.green,.blue,.black,.gray,.mint], center: .center, startRadius: 100, endRadius: 1000)
-        )
+        ).onAppear(perform: {
+            
+        })
         
     }
 }

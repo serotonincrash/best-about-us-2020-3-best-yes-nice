b2cd1c8d6d8da2b18fbccd2bf7b54ec7bee2e402
Sean Wong <derpydm@gmail.com>
2023-05-31 14:33:25 +0800

    staff

--- best-about-us-2020-3-best-yes-nice/SeanView.swift
+++ best-about-us-2020-3-best-yes-nice/SeanView.swift
@@ -10,6 +10,8 @@ import Font
 import Subsonic
 
 struct SeanView: View {
+    @State var isAnimating = true
+    
     var body: some View {
         ZStack {
             Color.clear
@@ -21,18 +23,25 @@ struct SeanView: View {
                     }
                     .scaledToFit()
                     .frame(width:200)
+                    .shadow(radius: 3)
+                    .scaleEffect(self.isAnimating ? 0.5 :1)
+                    .rotationEffect(self.isAnimating ? .degrees(0) : .degrees(360))
+                    .animation(.easeInOut(duration: 0.25).repeatForever())
                     .padding(.all)
                 Text("Sean Wong")
                     .font(.custom("Papyrus", size: 64))
-                    .foregroundColor(.red)
+                    .foregroundStyle(LinearGradient(colors: [.red,.green,.blue], startPoint: .leading, endPoint: .trailing))
+                    .shadow(radius: 3)
                     .bold()
                     .strikethrough()
                     .padding()
+                
             }
         }.background(
-            RadialGradient(colors: [.white,.blue,.green,.blue,.black,.gray,.mint], center: .center, startRadius: 100, endRadius: 1000)
+            RadialGradient(colors: [.white,.blue,.green,.blue,.black,.gray,.mint, .blue, .indigo, .yellow], center: .center, startRadius: 100, endRadius: 500)
         ).onAppear(perform: {
-            
+            isAnimating = false
+            play(sound: "rickroll.mp3")
         })
         
     }
